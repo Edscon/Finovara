4fd6039c8f96f70ff7c49d6835e46cf50e6a405a
Edscon <eduard.soler.11@gmail.com>
2025-05-25 22:40:26 +0200

    Commit

--- banking_api/views.py
+++ banking_api/views.py
@@ -26,33 +26,6 @@ async def institutions_api(request):
     except requests.RequestException as e:
         return HttpResponseServerError(f"Error obtenint institucions: {str(e)}")
 
-def altres(request):
-    #access_key = ApiKey.objects.filter(name='access_key').first()
-    access_key = get_access_token()['access']
-    print(access_key)
-    institutions = get_institutions(access_key, 'es')
-
-    #Para Pruebas
-    sandbox_institution_id = 'SANDBOXFINANCE_SFIN0000'
-
-    # Crear un acuerdo para una institución
-    agreement_data = create_agreement(
-        token=access_key,
-        institution_id= sandbox_institution_id
-    )
-
-    agreement_id = agreement_data.get('id')
-
-    # Crear una requisición de autenticación bancaria
-    requisition_data = create_requisition(
-        token=access_key,
-        agreement_id=agreement_id,
-        institution_id=sandbox_institution_id,
-        redirect_url= request.build_absolute_uri(reverse('dashboard')),
-        reference=agreement_id,  # Podrías usar un UUID si quieres algo único
-        user_language='ES'
-    )
-
 
 def app(request):
 
@@ -76,23 +49,6 @@ def dashboard_proba(request):
     
     account_data = {}
 
-    if requisition_id and access_key:
-        try:
-            requisition = get_requisition_details(access_key, requisition_id)
-            print(requisition)
-            accounts = requisition.get('accounts', [])
-            
-            for account_id in accounts:
-                balance = get_account_balance(access_key, account_id)
-                #transactions = get_account_transactions(access_key, account_id)
-
-                account_data[account_id] = {
-                    'balance': balance,
-                    #'transactions': transactions
-                }
-
-        except Exception as e:
-            print(f"Error carregant dades de compte: {e}")
 
     context = {
         'account_data': account_data,
