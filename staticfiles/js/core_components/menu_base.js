774150d762a2380585bf812e2f2653800d742746
Edscon <eduard.soler.11@gmail.com>
2025-05-31 00:27:09 +0200

    Commit

--- staticfiles/js/core_components/menu_base.js
+++ staticfiles/js/core_components/menu_base.js
@@ -1,25 +1,22 @@
-function MenuToggleWithSingleButton() {
-    const button = document.getElementById("menu-toggle-button");
-    const iconSpan = document.getElementById("menu-toggle-icon");
+function MenuButtonListener() {
+    
     const submenu = document.getElementById("div-menu-button");
+    const icon_menu = document.getElementById("svg-menu-button");
+    const icon_x_mark = document.getElementById("svg-x-mark-button");
+    
+    if (!submenu || !icon_menu || !icon_x_mark) return;
 
-    if (!button || !iconSpan || !submenu) return;
+    const toggleMenu = () => {
+        submenu.classList.toggle('hidden');
+        icon_menu.classList.toggle('hidden');
+        icon_x_mark.classList.toggle('hidden');
+    };
 
-    let isOpen = false;
+    icon_menu.addEventListener("click", toggleMenu);
+    icon_x_mark.addEventListener("click", toggleMenu);
 
-    // Almacena los SVG en variables (puedes cargarlos desde el DOM si los tienes como plantillas ocultas)
-    const svgMenu = `{% inline_svg 'svg/menu.svg' %}`;
-    const svgClose = `{% inline_svg 'svg/x-mark.svg' %}`;
-
-    button.addEventListener("click", () => {
-        isOpen = !isOpen;
-        submenu.classList.toggle("hidden");
-
-        iconSpan.innerHTML = isOpen ? svgClose : svgMenu;
-    });
 }
-
-MenuToggleWithSingleButton();
+MenuButtonListener();
 
 function toggleSubmenu() {
     document.querySelectorAll('.toggle-submenu').forEach(button => {
